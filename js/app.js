6febe8c71c422676be376d31f07dd16cd281b589
Your Name <santiago.perez777@gmail.com>
2022-09-27 13:28:41 +0200

    working on API project

---
+++ js/app.js
@@ -0,0 +1,30 @@
+const first = document.querySelector("#first");
+const last = document.querySelector("#last");
+const street = document.querySelector("#street");
+const phone = document.querySelector("#phone");
+const email = document.querySelector("#email");
+const picture = document.querySelector("#photo");
+const btn = document.getElementById("btn");
+btn.addEventListener("click", function () {
+  const randomPerson = getRandomPerson();
+  fillData(randomPerson);
+});
+
+const getRandomPerson = async () => {
+  const res = await axios.get(
+    "https://randomuser.me/api/?inc=name,location,phone,email,picture"
+  );
+  console.log(res.data.results[0]);
+  return res.data.results[0];
+};
+
+const fillData = async () => {
+  const res = await getRandomPerson();
+  first.innerHTML = res.name.first;
+  last.innerHTML = res.name.last;
+  street.innerHTML =
+    res.location.street.name + "," + " " + res.location.street.number;
+  phone.innerHTML = res.phone;
+  email.innerHTML = res.email;
+  picture.src = res.picture.medium;
+};
